3905e1fb099b93206298f0830fc86640df6286c7
ezequiel <5582340@alu.murciaeduca.es>
2023-01-23 08:10:37 +0100

    Actualizacion trabajo dia 19 enero

--- src/components/react-admin/admin.js
+++ src/components/react-admin/admin.js
@@ -1,15 +1,30 @@
 import { Admin, Resource } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
-import { UserList } from 'components/react-admin/users';
-import { PostList, PostEdit, PostCreate } from 'components/react-admin/posts';
-import { MigrationList } from 'components/react-admin/migrations';
 
-import PostIcon from '@mui/icons-material/Book';
+import { UserList } from 'components/react-admin/users';
+import { CustomerList, CustomerEdit, CustomerCreate } from 'components/react-admin/customers';
+
+//TODO eliminar las dos lineas siguientes
+import { PostList, PostEdit, PostCreate } from 'components/react-admin/posts';
+import { MigrationList, MigrationEdit, MigrationCreate } from 'components/react-admin/migrations';
+
+import CustomerIcon from '@mui/icons-material/SupportAgent';
 import UserIcon from '@mui/icons-material/Group';
+import PostIcon from '@mui/icons-material/Book';
 import MigrationIcon from '@mui/icons-material/Storage';
+
 import { AdminLayout } from 'components/react-admin/adminLayout';
 
-const dataProvider = jsonServerProvider(' http://webest.test/api/records');
+// para laravel Controllers
+ import jsonapiClient from 'ra-jsonapi-client';
+
+// para php-crud-api
+//import jsonServerProvider from 'ra-data-json-server';
+
+// para php-crud-api
+// const dataProvider = jsonServerProvider(' http://encuentro.test/api/records');
+
+// para laravel Controllers
+const dataProvider = jsonapiClient('http://encuentro.test/api');
 
 const RAdmin = () => (
   <Admin
@@ -17,7 +32,9 @@ const RAdmin = () => (
     dataProvider={dataProvider}
     layout={AdminLayout}
   >
-    <Resource name="migrations" list={MigrationList} icon={MigrationIcon} recordRepresentation="migration" />
+    <Resource name="customers" list={CustomerList} icon={CustomerIcon} edit={CustomerEdit} create={CustomerCreate} />
+    <Resource name="migrations"
+      list={MigrationList} icon={MigrationIcon} edit={MigrationEdit} create={MigrationCreate}/>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name" />
   </Admin>
